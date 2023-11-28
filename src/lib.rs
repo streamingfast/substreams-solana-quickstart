d04abb7f351ffe077e6e74666b56a6cd9c8e45a0
Stéphane Duchesneau <stephane.duchesneau@streamingfast.io>
2023-11-28 16:10:22 -0500

    add substreams SQL db_out module and sinkconfig

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 mod pb;
 
 use pb::sf::solana::block_meta::v1::BlockMeta;
-
+use substreams::Hex;
+use substreams_database_change::pb::database::DatabaseChanges;
+use substreams_database_change::tables::Tables as DatabaseChangeTables;
 use substreams_solana::pb::sol;
 
 #[substreams::handlers::map]
 fn map_block(block: sol::v1::Block) -> Result<BlockMeta, substreams::errors::Error> {
-    let mut block_height : Option<u64> = None;
+    let mut block_height: Option<u64> = None;
     if let Some(v) = block.block_height.as_ref() {
         block_height = Some(v.block_height)
     }
@@ -19,4 +21,17 @@ fn map_block(block: sol::v1::Block) -> Result<BlockMeta, substreams::errors::Err
         transaction_count: block.transactions.len() as u64,
         block_height,
     })
+}
+
+#[substreams::handlers::map]
+fn db_out(bm: BlockMeta) -> Result<DatabaseChanges, substreams::errors::Error> {
+    // Initialize changes container
+    let mut tables = DatabaseChangeTables::new();
+
+    tables
+        .create_row("block", [("hash", bm.hash)])
+        .set("parent_hash", bm.parent_hash)
+        .set("block_height", bm.slot)
+        .set("transaction_count", bm.transaction_count);
+    Ok(tables.to_database_changes())
 }
